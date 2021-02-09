49050ac25e59cd3d871aee02dfa5ac3deadc140b
Moaaz <imoaazahmed@gmail.com>
2021-02-09 15:49:58 +0400

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import { StrictMode } from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+
+// CSS
+import "./styles/masterstyle.css";
+
+ReactDOM.render(
+	<StrictMode>
+		<App />
+	</StrictMode>,
+	document.getElementById("root")
+);
